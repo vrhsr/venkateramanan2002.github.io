879e43387e8e8334b93c1bd56953a184d0fbcd22
venkate <mvrhsr@gmail.com>
2023-10-26 14:01:39 +0530

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,24 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="I'm Venkate Ramanan, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
+      <meta name="keywords" content="Venkate Ramanan, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Venkate Ramanan's Portfolio" />
+      <meta property="og:title" content="Portfolio | Venkate Ramanan - Full Stack Developer" />
+      <meta property="og:description"
+        content="I'm Venkate Ramanan, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
+      <meta property="og:url" content="https://jigarsable.vercel.app" />
+      <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
+      <link rel="canonical" href="https://jigarsable.vercel.app" />
+      <meta name="author" content="Venkate Ramanan" />
+      <meta name="language" content="English" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
+      <meta name="application-name" content="Portfolio | Venkate Ramanan - Full Stack Developer" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Venkate Ramanan - Full Stack Developer" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Venkate Ramanan - Full Stack Developer</title>
+    </>
+  )
+}
